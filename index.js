175dc8e460d84258122f337a7cd2a85b253c101b
Chukwuemeka (Emeka) Asogwa <menco23real@yahoo.com>
2021-06-29 05:43:59 +0000

    ✍🏽 initial commit

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-function sumItems(array) {
+const sumItems = function(array) {
   // Sum all the numbers in the array
-}
+  let summation = 0;
+  for (let value of array) {
+    if (Array.isArray(value)) {
+      summation += sumItems(value);
+    } else {
+
+      summation += (value);
+    }
+
+  }
+  return summation;
+};
 
 module.exports = sumItems;
